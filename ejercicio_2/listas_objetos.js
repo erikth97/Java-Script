0d329f86a8088f586d4a3ebfa9b290578986cc13
Erik Tamayo <erik.tamayohe@uanl.edu.mx>
2023-01-24 14:38:05 -0600

    Ejercicio 2 Listas, Objetos y Fechas
    Crea un nuevo archivo JS que contenga una lista con los siguientes elementos:
    
    - Tu nombre (string)
    
    - Tu edad (number)
    
    - ¿Eres desarrollador? (boolean)
    
    - Tu fecha de nacimiento (Date)
    
    - Tu libro favorito (Objeto con propiedades: titulo, autor, fecha, url)

---
+++ ejercicio_2/listas_objetos.js
@@ -0,0 +1,12 @@
+const erik= {
+    nombre: "Erik tamayo",
+    edad: 25,
+    desarrollador: true,
+    birth_day: new Date("november 10 1997"),
+    libro_favorito: {
+        titulo: "CleanCode",
+        autor: "Robert C. Martin",
+        fecha_publicacion: new Date ("august 1 2008"),
+        url_de_compra: "https://www.amazon.com.mx/Clean-Code-Handbook-Software-Craftsmanship/dp/0132350882"
+    }
+}
