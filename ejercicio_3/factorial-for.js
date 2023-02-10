79dae8bc9949302985682b3dce148e51772c69a3
Erik Tamayo <erik.tamayohe@uanl.edu.mx>
2023-02-10 00:55:36 -0600

    Ejercicio 3 For, While, Break
    Para este ejercicio realizamos tres programas diferentes con las siguientes funciones:
    Crea los siguientes archivos JS:
    - factorial-for.js -> Este archivo debe calcular el factorial de 10 utilizando un solo bucle for
    - factorial-while.js -> Este archivo debe calcular el factorial de 10 utilizando un bucle while
    - factorial-break.js -> Este archivo debe calcular el factorial de 10 utilizando un bucle while, una bifurcación if y una sentencia break

---
+++ ejercicio_3/factorial-for.js
@@ -0,0 +1,9 @@
+//factorial-for.js -> Este archivo debe calcular el factorial de 10 utilizando un solo bucle for
+let factorial = 1;
+
+for (let i = 1; i <= 10; i++){
+
+    factorial = factorial * i; 
+
+}
+console.log("El factorial de 10 es: " + factorial);
